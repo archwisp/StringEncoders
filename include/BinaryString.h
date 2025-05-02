71c6d512ec6f9c244694d23f2db184e985d998c4
Archwisp <archwisp@gmail.com>
2025-05-02 12:00:01 -0700

    Updated BinaryString header

--- include/BinaryString.h
+++ include/BinaryString.h
@@ -1,3 +1,5 @@
+void byte_to_bits(unsigned const char byte, char *bit_str);
 void test_byte_to_bits();
+void byte_array_to_bits(char *output, const int output_len, const char *input, const int input_len);
 void test_byte_array_to_bits();
 void print_byte_array_bits(const char *input, const unsigned int input_len);
